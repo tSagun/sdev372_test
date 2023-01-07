567188510782f0c0354228da5dda565483653a5a
TobieSagun <sagun.tobie@student.greenriver.edu>
2023-01-06 18:27:17 -0800

    reverted to a previous version of the Point class. changed (x, y) coordinates to (x, y, z) coordinates

--- testing/src/changes/Point.java
+++ testing/src/changes/Point.java
@@ -6,11 +6,13 @@ public class Point
 {
     private double x;
     private double y;
+    private double z;
 
-    public Point(double x, double y)
+    public Point(double y, double x, double z)
     {
         this.x = x;
         this.y = y;
+        this.z = z;
     }
 
     public double getX()

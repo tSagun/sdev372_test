a12e8530ec3113796e9de2d4221075b73b8dc57d
TobieSagun <sagun.tobie@student.greenriver.edu>
2023-01-06 18:19:47 -0800

    added equals() and hashcode()

--- testing/src/changes/Point.java
+++ testing/src/changes/Point.java
@@ -1,5 +1,7 @@
 package changes;
 
+import java.util.Objects;
+
 public class Point
 {
     private double x;
@@ -35,5 +37,26 @@ public class Point
     public String toString()
     {
         return "Point{}";
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o)
+        {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass())
+        {
+            return false;
+        }
+        Point point = (Point) o;
+        return Double.compare(point.x, x) == 0 && Double.compare(point.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(x, y);
     }
 }
